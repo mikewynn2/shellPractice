68fcca08cae77ec02f210b69a869ce86beaecc34
Mike Wynn <wynn@mikes-mbp.lan>
2024-03-19 18:40:23 -0700

    update count number

--- test1.py
+++ test1.py
@@ -2,10 +2,10 @@ import time
 
 
 print("This is test.py, the first script:")
-print("we will now print 0-9:")
+print("we will now print 0-20:")
 time.sleep(2)
 
 
-for i in range(10):
+for i in range(21):
     print(i)
 
